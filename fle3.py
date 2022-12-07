2958513e8d0d5f7942a556bd7f6e7d42b4835d21
superKatusha <57723627+superKatusha@users.noreply.github.com>
2022-12-07 18:56:45 +0300

    Возможно Тима

--- fle3.py
+++ fle3.py
@@ -1,8 +1,6 @@
-from itertools import product, permutations
-from math import factorial
-# https://stepik.org/lesson/552237/step/9?auth=login&unit=545965
-lst1 = sorted(map(lambda x: ''.join(x), permutations('МАРИ', 4)))
-lst2 = sorted(map(lambda x: ''.join(x), product('ИНА', repeat=4)))
-print(len(lst1), len(lst2))
-print()
-print(lst1.index('МАРИ') * 81 + lst2.index('АННА') + 1)
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
